16bd7ad2141aac93e532a7d0f35e486c3e01435d
Bimal shukla <2001bimal@gmail.com>
2022-04-15 12:38:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,18 +20,15 @@ res.sendFile(__dirname +"/signup.html" );
 const mailchimp = require("@mailchimp/mailchimp_marketing");
 
 mailchimp.setConfig({
-  apiKey: "c172b3839e024b3fbcabf6636d4e1ca6-us14",
-  server: "us14",
+  apiKey: "###",
+  server: "usX",
 });
 
 
 
-// https://mailchimp.com/developer/marketing/guides/create-your-first-audience/
-// https://youtu.be/Gjk25N7WFkI?t=1370
-
 app.post("/", function(req,res){
 
-  //const listId = "cd3047f3e2";
+ 
   const firstName= req.body.fName;
   const lastName= req.body.lName;
   const email = req.body.email;
@@ -49,13 +46,13 @@ app.post("/", function(req,res){
 
 
 const jsonData= JSON.stringify(data);
-const url= "https://us14.api.mailchimp.com/3.0/lists/cd3047f3e2";
+const url= "https://usX.api.mailchimp.com/3.0/lists/List_ID";
 
   const options = {
     method: "POST",
     body: data,
     headers:{
-    Authorization : "auth c172b3839e024b3fbcabf6636d4e1ca6-us14"
+    Authorization : "auth ###"
   }
   };
 
@@ -95,8 +92,3 @@ app.listen(process.env.PORT || 3000, function(){
   console.log("server is running on port 3000!");
 });
 
-// api key
-// c172b3839e024b3fbcabf6636d4e1ca6-us14
-
-//  list id
-//  cd3047f3e2
